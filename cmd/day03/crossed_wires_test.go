19c18a4bb7b500cd9a5ae36fff2449d3fc19d144
Michael Wegner <miwn02@gmail.com>
2020-01-03 17:39:47 +0100

    code for day 3

---
+++ cmd/day03/crossed_wires_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestPuzzle1(t *testing.T) {
+	cases := []struct {
+		wire1    string
+		wire2    string
+		distance int
+	}{
+		{"R8,U5,L5,D3", "U7,R6,D4,L4", 6},
+		{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159},
+		{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135},
+	}
+	for _, c := range cases {
+		got := puzzle1(c.wire1, c.wire2)
+		if got != c.distance {
+			t.Errorf("puzzle1(%q, %q) == %v, want %v", c.wire1, c.wire2, got, c.distance)
+		}
+	}
+}
+
+func TestPuzzle2(t *testing.T) {
+	cases := []struct {
+		wire1    string
+		wire2    string
+		distance int
+	}{
+		{"R8,U5,L5,D3", "U7,R6,D4,L4", 30},
+		{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 610},
+		{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 410},
+	}
+	for _, c := range cases {
+		got := puzzle2(c.wire1, c.wire2)
+		if got != c.distance {
+			t.Errorf("puzzle2(%q, %q) == %v, want %v", c.wire1, c.wire2, got, c.distance)
+		}
+	}
+}
